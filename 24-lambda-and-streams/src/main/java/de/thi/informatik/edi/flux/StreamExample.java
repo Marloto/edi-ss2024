afefeebc9955d52813d95f8e549e2399d9eaa9ec
Marloto <marloto@codestack.de>
2024-04-29 11:22:59 +0200

    Examples from 29.04.2024

---
+++ 24-lambda-and-streams/src/main/java/de/thi/informatik/edi/flux/StreamExample.java
@@ -0,0 +1,13 @@
+package de.thi.informatik.edi.flux;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class StreamExample {
+	public static void main(String[] args) {
+		//List.of("1", "21", "2", "4", "3", "42").forEach(System.out::println);
+		List<String> list = List.of("1", "21", "2", "4", "3", "42");
+		
+	}
+}
